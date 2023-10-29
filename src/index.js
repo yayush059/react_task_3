4504f68bd2cbfddbda2ea6b230d3c60823ff2321
yayush059 <108602300+yayush059@users.noreply.github.com>
2023-10-29 22:52:23 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import {Color} from './components/color';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Color color1="#fe0000" color2="#00ff00"  color3="#0000fe" color4="#ffff00" color5="#fe00fe" color6="#00ffff" color7="#fea500" color8="#81007f" color9="#ffc0cb" color10="#008000" color11="#fe6347" color12="#00cfd1" color13="#8b4413" color14="#ff8b00" color15="#4683b4" color16="#fed700"></Color>
+);
+
+
+reportWebVitals();
